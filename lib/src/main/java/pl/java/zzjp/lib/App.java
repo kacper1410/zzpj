a517be4b3dc7ab3e506fd6bc50e0f11403a155a3
Kacper <kacper.swiercz00@gmail.com>
2021-03-06 13:30:46 +0100

    Add projects

---
+++ lib/src/main/java/pl/java/zzjp/lib/App.java
@@ -0,0 +1,13 @@
+package pl.java.zzjp.lib;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
