548cf54ef5838991aa31f4acd1947d8be96fc1e6
Kacper <kacper.swiercz00@gmail.com>
2021-03-06 17:57:50 +0100

    Add profiles to pom.xml

--- app/src/main/java/pl/java/zzjp/app/App.java
+++ app/src/main/java/pl/java/zzjp/app/App.java
@@ -2,12 +2,30 @@ package pl.java.zzjp.app;
 
 import pl.java.zzjp.lib.Person;
 
-public class App 
-{
-    public static void main( String[] args )
-    {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class App {
+
+    public static void main(String[] args) {
+
         Person person = new Person();
-        person.setName("Adam");
+        person.setName(getClientNameProperty());
         person.greet();
     }
+
+    public static String getClientNameProperty() {
+        Properties properties = new Properties();
+        String propFileName = "config.properties";
+        InputStream inputStream = App.class.getClassLoader().getResourceAsStream(propFileName);
+        try {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return properties.getProperty("clientName");
+    }
+
 }
